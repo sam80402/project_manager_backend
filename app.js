2f06c1c8a18cbd0defb019d26cf03d31b48f8313
sam80402 <sam50308@gmail.com>
2024-07-06 16:06:54 +1000

    add api access function

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ require('dotenv').config();
 const authRoute = require('./routes').auth;
 const customerRoute = require('./routes').customer;
 const caseRoute = require('./routes').case;
+const checkApiKey = (req, res, next) => {
+  const apiKey = req.headers['x-api-key'];
+  if (!apiKey || apiKey !== process.env.API_KEY) {
+    return res.status(401).json({ message: 'Invalid or missing API Key' });
+  }
+  next();
+};
+
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/Project_ManagerDB")
@@ -21,9 +29,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+
+// 將中間件應用到所有路由
+app.use(checkApiKey);
+
+
+
 app.use("/api/auth", authRoute);
 app.use("/api/customer", customerRoute);
 app.use("/api/case", caseRoute);
+
+
 
 app.listen(8080,() => {
     console.log('port 8080 is running...');
